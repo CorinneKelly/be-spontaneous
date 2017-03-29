647d5f809047450f226c10db70b91998aaf9b943
CorinneKelly <corinnemariekelly@gmail.com>
2017-03-29 18:11:27 -0400

    trying to fix the drag drop jumplink

--- skyscanner/app/controllers/application_controller.rb
+++ skyscanner/app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::API
 	include HTTParty
 
 	def serve
+
 		destination = params[:destination]
+		byebug
 		response = HTTParty.get("http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/US/USD/en-US/NYC/#{destination}/anytime/?apiKey=#{Rails.application.secrets.flight_api}").to_json
 		render :json => response
 	end
