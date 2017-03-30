b066266094852016e5b8e9fe945237effe3dfe40
CorinneKelly <corinnemariekelly@gmail.com>
2017-03-30 19:34:04 -0400

    got geolocation to default to NY if haters decide to bock their location

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var originLat
 var originLong
 $(() => {
-	loading_page.html
 	navigator.geolocation.getCurrentPosition(function(position) {
 		originLat = position.coords.latitude
 		originLong = position.coords.longitude
+		$('#loading').remove()
+		Google.init()
+
+	}, function() {
+		originLong = -74.0059
+		originLat = 40.7128
+		$('#loading').remove()
 		Google.init()
 	})
 })
--- views/FlightsView.js
+++ views/FlightsView.js
@@ -15,7 +15,7 @@ class FlightsView {
 		return `
 		<div class="flight-quote"
 			<h3>${quote.price}</h3>
-			<p><strong>${quote.inbound.destination}</strong> <img src="dual_planes.svg" height="60" width="60"> <strong>${quote.outbound.destination}</strong></p>
+			<p>${quote.inbound.destination} <img src="dual_planes.svg" height="30" width="30"> ${quote.outbound.destination}</p>
 			<p>${quote.direct}</p>
 			<p>${quote.outbound.departureDate} to ${quote.inbound.departureDate} </p>
 			<p>Carriers: ${quote.inbound.carriers} & ${quote.outbound.carriers} </p>
