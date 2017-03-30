d0bccefb0806ea85cfa19d8ed1fbf9e060131d72
CorinneKelly <corinnemariekelly@gmail.com>
2017-03-29 21:43:03 -0400

    params successfully transmit to rails api

--- skyscanner/app/controllers/application_controller.rb
+++ skyscanner/app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
 	include HTTParty
 
-	def serve
+	def incoming
 
-		destination = params[:destination]
-		byebug
+		destination = params[:destination].gsub("x", ".").gsub("y", "-")
 		response = HTTParty.get("http://partners.api.skyscanner.net/apiservices/browsedates/v1.0/US/USD/en-US/NYC/#{destination}/anytime/?apiKey=#{Rails.application.secrets.flight_api}").to_json
 		render :json => response
 	end
+
 end
