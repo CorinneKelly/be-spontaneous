826be8a1fe6f193458593dce0ce29cb85e431aeb
Noah Schechter <noahschechter7@gmail.com>
2017-03-30 14:27:36 -0400

    formatted api data and made it presentable. also added roundtrip

--- controllers/FlightActionsController.js
+++ controllers/FlightActionsController.js
@@ -1,14 +1,9 @@
 class FlightActionsController {
 	constructor(data) {
 		window.location.href = "#flights-wrapper"
-		flight = new Flight(data)
-
-
+		let flight = new Flight(data)
+		debugger;
 		//flight.getNameFromId(data.Carriers, "CarrierId", 1793)
 		//flight.getNameFromId(data.Places, "PlaceId", 1793)
-	}
-
-	render() {
-		
 	}
 }
--- models/Flight.js
+++ models/Flight.js
@@ -1,18 +1,75 @@
 class Flight {
 	constructor(data) {
-		this.carriers = data.carriers
-		this.places = data.places
-		this.quotes = data.quotes
+		this.carriers = data.Carriers
+		this.places = data.Places
+		this.quotes = data.Quotes
 	}
 
 	getNameFromId(category, catName, id) {
 		let name = ""
 		category.forEach((c) => {
-			console.log("looping")
 			if (c[catName] == id)
 				name = c.Name
 		})
 		return name
+	}
+
+	getQuotes() {
+		return this.quotes
+	}
+
+	getQuoteData(id) {
+		let quote;
+		this.getQuotes().forEach((q) => {
+			if (q.QuoteId == id)
+				quote = q
+		})
+		if (quote) {
+			let outBoundPlanes = quote.OutboundLeg.CarrierIds.map((c) => {
+				return this.getNameFromId(this.carriers, "CarrierId", c)
+			})
+			let inBoundPlanes = quote.InboundLeg.CarrierIds.map((c) => {
+				return this.getNameFromId(this.carriers, "CarrierId", c)
+			})
+			return {
+				id: quote.QuoteId, price: `$$$$$${quote.MinPrice}`,
+				direct: quote.Direct, 
+				outbound: {
+					departureDate: this.formatDate(quote.OutboundLeg.DepartureDate),
+					departureTime: this.formatTime(quote.OutboundLeg.DepartureDate),
+					destination: this.getNameFromId(this.places, "PlaceId", quote.OutboundLeg.DestinationId),
+					origin: this.getNameFromId(this.places, "PlaceId", quote.OutboundLeg.OriginId),
+					carriers: outBoundPlanes
+				},
+				inbound: {
+					departureDate: this.formatDate(quote.InboundLeg.DepartureDate),
+					departureTime: this.formatTime(quote.InboundLeg.DepartureDate),
+					destination: this.getNameFromId(this.places, "PlaceId", quote.InboundLeg.DestinationId),
+					origin: this.getNameFromId(this.places, "PlaceId", quote.InboundLeg.OriginId),
+					carriers: inBoundPlanes
+				} 
+			}
+
+		} else {
+			return "No Quote with Id: " + id
+		}
+	}
+
+	formatTime(dateTime) {
+		let timeFromObject = new Date(dateTime)
+		if (timeFromObject.getHours() > 12) {
+			var flightTime = (timeFromObject.getHours() - 12) + ":" + timeFromObject.getMinutes() + "PM"
+		} else {
+			var flightTime = timeFromObject.getHours() + ":" + timeFromObject.getMinutes() + "AM"
+		}
+		return flightTime
+	}
+
+	formatDate(dateTime) {
+		let dateFromObject = new Date(dateTime)
+		let monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+		let stringDate = monthArray[dateFromObject.getMonth()] + ' ' + dateFromObject.getDate() + ' ' +  dateFromObject.getFullYear()
+		return stringDate
 	}
 
 	static find(destination) {
